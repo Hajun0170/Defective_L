87c214d0994f7f824755ae912a1fbb54e7e54a28
박준하 <149313406+Hajun0170@users.noreply.github.com>
2025-11-03 12:00:26 +0900

    Weapon Logic

---
+++ A_Defective_L/Assets/Scripts/Weapon/WeaponId.cs
@@ -0,0 +1,10 @@
+public enum WeaponId
+{
+    None = 0,
+    Sword = 1,        // 근접(오른팔)
+    Drill = 2,        // 근접(오른팔)
+    Shield = 3,       // 원거리계 패링/방어(왼팔 처리)
+    Gatling = 10,     // 원거리(왼팔)
+    Knife = 11,       // 원거리(왼팔)
+    RocketPunch = 12  // 원거리(왼팔)
+}
